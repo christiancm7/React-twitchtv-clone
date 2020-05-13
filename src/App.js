17cbfb03343fc3f8f3fd6e8906c1f67495749e29
Christian <atticuschris@yahoo.com>
2020-05-13 10:03:19 -0500

    portal setup and done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
-import StreamCreate from './components/streams/StreamCreate'
-import StreamDelete from './components/streams/StreamDelete'
-import StreamEdit from './components/streams/StreamEdit'
-import StreamList from './components/streams/StreamList'
-import StreamShow from './components/streams/StreamShow'
-import Header from './components/Header'
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import StreamCreate from "./components/streams/StreamCreate";
+import StreamDelete from "./components/streams/StreamDelete";
+import StreamEdit from "./components/streams/StreamEdit";
+import StreamList from "./components/streams/StreamList";
+import StreamShow from "./components/streams/StreamShow";
+import Header from "./components/Header";
+import history from "./history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
+          <Route path="/streams/delete/:id" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from "../apis/streams";
+import history from "../history";
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -29,6 +31,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     type: CREATE_STREAM,
     payload: response.data,
   });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -42,11 +45,13 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`streams/${id}`, formValues);
+  const response = await streams.patch(`streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
 };
